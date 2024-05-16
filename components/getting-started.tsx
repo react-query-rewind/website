e7f557df78ab176a0ed6b7c78fab5920426d7794
John Dunn <johnwdunn20@gmail.com>
2024-05-16 10:37:29 -0700

    updated text

--- components/getting-started.tsx
+++ components/getting-started.tsx
@@ -77,7 +77,7 @@ export default function GettingStarted() {
                     <svg className="w-3 h-3 fill-current text-emerald-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>Find the <span className='italic'>RQRewind</span> tab</span>
+                    <span>Find the <span className='italic'>React Query Rewind</span> tab</span>
                   </li>
                 </ul>
               </div>
